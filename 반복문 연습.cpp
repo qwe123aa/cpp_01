5416942c0048351a207bbd12405cd9538d419f25
dpfl <love05061114@gmail.com>
2023-03-14 09:59:18 +0900

    완벽한 구구단

--- 반복문 연습.cpp
+++ 반복문 연습.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
+	
+	for (int i = 2; i < 10; i++) {
+		for (int j = 1; j < 10; j++) {
+			printf("%d x %d = %d\n", i, j, i * j);
+		}
+	}
+
 
 	for (int i = 2; i < 10; i++) {
 		printf("%d x 1 = %d\n", i, i*1);
