dbebd005ff73fc71af194d90db43802e9422305b
dpfl <love05061114@gmail.com>
2023-03-14 09:55:45 +0900

    구구단 약간 효율적으로

--- 반복문 연습.cpp
+++ 반복문 연습.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
+
+	for (int i = 2; i < 10; i++) {
+		printf("%d x 1 = %d\n", i, i*1);
+		printf("%d x 2 = %d\n", i, i*2);
+		printf("%d x 3 = %d\n", i, i*3);
+		printf("%d x 4 = %d\n", i, i*4);
+		printf("%d x 5 = %d\n", i, i*5);
+		printf("%d x 6 = %d\n", i, i*6);
+		printf("%d x 7 = %d\n", i, i*7);
+		printf("%d x 8 = %d\n", i, i*8);
+		printf("%d x 9 = %d\n", i, i*9);
+	}
+
 	printf("2 x 1 = 2\n");
 	printf("2 x 2 = 4\n");
 	printf("2 x 3 = 6\n");
