7cdc5d04bb3d8430a7f047f6d9dae5481aecfeac
dpfl <love05061114@gmail.com>
2023-03-16 10:15:41 +0900

    namespace

--- cpp기본.cpp
+++ cpp기본.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+using namespace std;	//이름공간
+
 int main(void) {
-	std::cout << "Hello World\n" << std::endl;
+	cout << "Hello World\n" << endl;
 	//std::cout :출력을 위한 객체
 	//<< : 출력 연산자
 	//std::endl 개행(줄바꿈) 객체
