7579224a711a699e217a5697951b8bc123fe3ddc
dpfl <love05061114@gmail.com>
2023-03-16 10:08:41 +0900

    개행을 해주는 endl 객체

--- cpp기본.cpp
+++ cpp기본.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int main(void) {
-	std::cout << "Hello World\n";
+	std::cout << "Hello World\n" << std::endl;
 	//std::cout :출력을 위한 객체
 	//<< : 출력 연산자
+	//std::endl 개행(줄바꿈) 객체
 	return 0;
 }
