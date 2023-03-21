e15afacf28087a152abbe973c39eec2ce98b4fc1
dpfl <love05061114@gmail.com>
2023-03-21 09:05:57 +0900

    참조(reference)

--- 참조형 변수.cpp
+++ 참조형 변수.cpp
@@ -5,8 +5,9 @@ using namespace std;	//이름공간
 int main(void) {
 	int a = 30;
 	
-	int* pa = &a;		//pa :a의 주솟값
-	*pa = 100;			//pa가 가리키는 대상(a) : 100
+	int& ra = a;		//pa :a의 주솟값
+	ra = 100;			//pa가 가리키는 대상(a) : 100
+	//참조 = 포인터
 
 	cout << "a의 값은 " << a << endl;
 
