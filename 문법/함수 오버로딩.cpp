4ffe487ec4e6c89d300b27ed288014de23b8c1e7
dpfl <love05061114@gmail.com>
2023-03-21 10:00:58 +0900

    참조, 함수 오버로딩 전

---
+++ 문법/함수 오버로딩.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int sum_int(int a, int b) {
+	return a + b;
+}
+
+float sum_float(float a, float b) {
+	return a + b;
+}
+
+int main(void) {
+	cout << sum_int(1, 2) << endl;
+	cout << sum_float(1.1f, 2.2f) << endl;
+	return 0;
+}
