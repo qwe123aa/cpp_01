1dec441213218b9987695f76cc6a9c49006b0f70
thdud__ <love05061114@gmail.com>
2023-03-23 10:12:12 +0900

    C스타일 strcpy

--- 문법/String 복사.cpp
+++ 문법/String 복사.cpp
@@ -1,13 +1,12 @@
 ﻿#include <stdio.h>
 #include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
 
 int main(void) {
 	char str[30] = {0,};
 	char str2[30] = "c_insert";
 
-	for (int i = 0; str2[i] != '\0'; i++) {
-		str[i] = str2[i];
-	}
+	strcpy(str, str2);	//str에다가 str2에 있는 문자열을 넣겠다 라는 뜻
 
 	printf("%s", str);
 
