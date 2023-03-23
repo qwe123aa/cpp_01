66ba9c09718037bb7ba16744d18aca130bece89a
thdud__ <love05061114@gmail.com>
2023-03-23 10:47:29 +0900

    c스타일 str len() 문자열 길이

--- 문법/String 추가.cpp
+++ 문법/String 추가.cpp
@@ -1,15 +1,10 @@
 ﻿#include <iostream>
 #include <string.h>
-#define _CRT_SECURE_NO_WARNINGS
+
 
 int main(void) {
-	std::string str1;
-	std::string str2 = "c++_insert";
+	char str[30] = "Jiwoo";
 
-	str1 = str2;
-	std::cout << str1 << std::endl;
-	
-
-
+	printf("%d", strlen(str));
 	return 0;
 }
