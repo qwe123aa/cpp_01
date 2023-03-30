e6b71a5af48af2eda52b43eed01187db3bdd7541
thdud__ <love05061114@gmail.com>
2023-03-30 10:48:35 +0900

    구조체 짬

--- 문법/클래스 기본.cpp
+++ 문법/클래스 기본.cpp
@@ -4,20 +4,33 @@
 using namespace std;
 
 //여러 data들을 하나의 의미있는 구조체로 묶어서 관리
-struct drink {
-	string subun;
-	string chumga;
-
+//일반적으로 구조체, 클래스의 단어의 첫 알파벳을 대문자로
+struct student {
+	string name;
+	//성능 때문에 문자열로 하지 않음
+	//일반적으로 문자열은 정수 데이터보다 많은 메모리 공간을 요구하며
+	//정수는 비교연산을 한 번에 할 수 있으나 문자열은 글자 수 만큼 반복하여 비교해야함
+	int id;
+	int age;
+	//0 : 남자  //1 : 여자
+	int sex;		//가독성과 유지보수를 위해 열거형(enum)으로 하는 것을 추천
+	string dep;
 };
 
 int main(void) {
-	//struct drink가 자료형, c++에서는 drink만 써도 됨
-	struct drink 딸기우유;
-	딸기우유.subun = "서울우유";	
-	딸기우유.chumga = "킹스베리";
+	//(struct student) 자료형(c++에서는 student만 써도 됨)
+	struct student std1;
+	std1.name = "임소영";
+	std1.id = 2115;
+	std1.age = 18;
+	std1.sex = 1;
+	std1.dep = "소프트웨어과";
 
-	cout << "수분 : " << 딸기우유.subun << "  첨가물 : " << 딸기우유.chumga << endl;
-
+	cout << "이름 : " << std1.name <<endl;
+	cout <<"학번 : " << std1.id << endl;
+	cout <<"나이 : " << std1.age << endl;
+	cout <<"성별 : " << std1.sex << endl;
+	cout <<"학과 : " << std1.dep << endl;
 
 	return 0;
 }
