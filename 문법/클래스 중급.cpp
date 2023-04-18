5f7c2d2fb3d662d39cee4acb1d842e982cac07bd
thdud__ <love05061114@gmail.com>
2023-04-18 10:12:09 +0900

    동적할당

--- 문법/클래스 중급.cpp
+++ 문법/클래스 중급.cpp
@@ -1,19 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+#include <string.h>
 
 using namespace std;
 
 class MString {
 public:
 	MString(const char* str) {
-		c_str_ = str;
+		unsigned int strLength = strlen(str);
+		//필요한 길이만큼 문자열 동적할당
+		c_str_ = new char[strLength+1];	//'\0' (널문자) 공간 +1
+		strcpy(c_str_, str);
 	}
 
 	
 	unsigned int size() { return size_; }
-	const char* c_str() { return c_str_; }
+	char* c_str() { return c_str_; }
 private:
 	unsigned int size_;
-	const char* c_str_; //문자열을 가리키는 주소
+	char* c_str_; //문자열을 가리키는 주소
 
 };
 
