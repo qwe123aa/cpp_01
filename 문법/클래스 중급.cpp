6aa5d02314a6d4d5fce60146c89b29ba5604be34
thdud__ <love05061114@gmail.com>
2023-04-25 08:50:06 +0900

    생성자 소멸자 호출되는지 확인

--- 문법/클래스 중급.cpp
+++ 문법/클래스 중급.cpp
@@ -11,11 +11,13 @@ public:
 		//필요한 길이만큼 문자열 동적할당
 		c_str_ = new char[strLength+1];	//'\0' (널문자) 공간 +1
 		strcpy(c_str_, str);
+		cout << "MString 생성자 호출 완료" << endl;
 	}
 
 	//소멸자(destructor)
 	~MString() {
 		delete[] c_str_;	//생성자에 동적할당한 공간 해제
+		cout << "MString 소멸자 호출 완료" << endl;
 	}
 	
 	unsigned int size() { return size_; }
