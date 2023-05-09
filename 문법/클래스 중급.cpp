d7dd6814ba20b7fd9793a0d319170a848578fac7
thdud__ <love05061114@gmail.com>
2023-05-09 10:17:29 +0900

    복사생성자

--- 문법/클래스 중급.cpp
+++ 문법/클래스 중급.cpp
@@ -6,12 +6,20 @@ using namespace std;
 
 class MString {
 public:
+	//일반적인 생성자
 	MString(const char* str) {
 		unsigned int strLength = strlen(str);
 		//필요한 길이만큼 문자열 동적할당
 		c_str_ = new char[strLength+1];	//'\0' (널문자) 공간 +1
 		strcpy(c_str_, str);
 		cout << "MString 생성자 호출 완료" << endl;
+	}
+
+	//복사생성자(별도로 정의하지 않으면 컴파일러가 아래와 같은 복사 생성자를 만들어냄)
+	MString(const MString& rhs)
+		: size_(rhs.size_), c_str_(rhs.c_str_)
+	{
+		cout << "복사생성자 호출" << endl;
 	}
 
 	//소멸자(destructor)
@@ -29,8 +37,11 @@ private:
 };
 
 int main(void) {
+	//일반적인 생성자 호출
 	MString str1 = MString("Aitai");
 	cout << str1.c_str() << endl;
+
+	MString str2 = str1;	//복사생성자
 
 	return 0;
 }
