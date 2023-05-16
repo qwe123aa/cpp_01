7ec15744e73a4bebb398db8111cde82669e7b963
Yu Jung Eun <0hh.0553@gmail.com>
2023-05-16 09:37:30 +0900

    복사생성자로 깊은 복사

--- 문법/복사생성자.cpp
+++ 문법/복사생성자.cpp
@@ -15,7 +15,9 @@ public:
 	}
 
 	//복사생성자(얕은 복사로 인해 문제점이 발생)
-	Munja(const Munja& m) : str_(m.str_) {
+	Munja(const Munja& m) {
+		str_ = new char[strlen(m.str_) + 1];
+		strcpy(str_, m.str_);	//str의 내용을 str_에 복사
 		cout << "복사생성자 호출" << endl;
 	}
 
