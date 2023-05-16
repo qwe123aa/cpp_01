fbea235ccd4eaef974850f074317d202bec31cba
Yu Jung Eun <0hh.0553@gmail.com>
2023-05-16 09:04:40 +0900

    일반생성자부터 만들어보기

--- 문법/깊은복사, 얕은복사.cpp
+++ 문법/깊은복사, 얕은복사.cpp
@@ -10,10 +10,19 @@ int main(void) {
 
 	//ptr2는 별도의 동적할당을 하지 않고 ptr1의 주솟값만 복사한다.
 	//이러한 복사를 얕은복사라고 한다(shallow copy)
-	int* ptr2 = ptr1;
+	//int* ptr2 = ptr1;
+
+	//ptr2는 별도의 공간을 동적할당한 후 ptr1이 가리키는 값들을 복사한다
+	//이러한 복사를 깊은 복사라고 한다(deep copy)
+	int* ptr2 = new int[3];
+	for (int i = 0; i < 3; i++) {
+		ptr2[i] = ptr1[i];
+	}
 
 	//얕은 복사를 하면 원본 데이터에 종속된다.
 	//즉, ptr2[1]도 100으로 바뀌게 된다.
+	//깊은 복사를 하면 고유의 공간을 가지므로 
+	//ptr2[1]은 100으로 바뀌지 않는다.
 	ptr1[1] = 100;
 
 	for (int i = 0; i < 3; i++) {
@@ -25,5 +34,9 @@ int main(void) {
 		cout << ptr2[i] << "\t";
 	}
 
+	delete[] ptr1;
+	delete[] ptr2;
+
+	
 	return 0;
 }
---
+++ 문법/복사생성자.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Munja {
+public:
+	//일반 생성자(깊은 복사)
+	Munja(const char* str) {
+		//3("abc")+1('\0')
+		str_ = new char[strlen(str) + 1];
+		strcpy(str_, str);	//str의 내용을 str_에 복사
+
+		cout << "일반생성자 호출" << endl;
+	}
+
+	//소멸자
+	~Munja() {
+		delete[] str_;
+		cout << "소멸자 호출" << endl;
+	}
+
+private:
+	char* str_;
+
+};
+
+int main(void) {
+	Munja m1 = Munja("abc");
+}
