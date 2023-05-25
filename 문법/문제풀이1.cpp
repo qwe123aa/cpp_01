cfc6e8efa005b679c76259fe86cdbc10297fd7f6
thdud__ <love05061114@gmail.com>
2023-05-25 09:59:03 +0900

    문제풀이 1-1 얕은 복사

---
+++ 문법/문제풀이1.cpp
@@ -0,0 +1,27 @@
+﻿class IntArray {
+public:
+	IntArray(int* arr, int size) {
+		arr_ = new int[size];
+		size_ = size;
+		for (int i = 0; i < size; i++) {
+			arr_[i] = arr[i];
+		}
+
+	}
+
+	//얕은복사 생성자
+	IntArray(const IntArray& rhs) : size_(rhs.size_), arr_(rhs.arr_) {
+	}
+
+private:
+	int* arr_;
+	int size_;	//arr의 크기
+};
+
+int main(void) {
+	int arr[] = { 1,2,3 };
+	IntArray ia = IntArray(arr, sizeof(arr)/sizeof(*arr));
+	IntArray ca = ia;	//복사생성자
+
+	return 0;
+}
