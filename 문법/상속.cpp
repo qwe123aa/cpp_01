1b948e180f615d5af0734159f66ecfd93ee93fea
thdud__ <love05061114@gmail.com>
2023-06-08 10:16:12 +0900

    상속과 부모생성자

---
+++ 문법/상속.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+	Animal() { cout << "Animal 생성자()" << endl; };
+	~Animal() { cout << "Animal 소멸자()" << endl; };
+};
+
+class Rabbit : public Animal {
+public:
+	//매개변수가 없는 부모 생성자가 자동으로 먼저 호출된다
+	Rabbit() { cout << "Rabbit 생성자()" << endl; };
+	~Rabbit() { cout << "Rabbit 소멸자()" << endl; };
+};
+
+int main(void) {
+	Rabbit* rabbit = new Rabbit();
+	delete rabbit;
+}
