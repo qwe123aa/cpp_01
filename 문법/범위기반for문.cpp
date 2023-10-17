7910f4575624c5bdd62bb6b20169d80ddbb5adef
thdud__ <love05061114@gmail.com>
2023-10-17 09:59:34 +0900

    auto : 자료형 자동설정

--- 문법/범위기반for문.cpp
+++ 문법/범위기반for문.cpp
@@ -7,8 +7,13 @@ using namespace std;
 int main(void) {
 	double arr[4] = { 1.1,2.2,3.3,4.4 };
 
+	auto a = 4;
+
 	for (double x : arr) {
 		cout << x << endl;
 	}
+
+	for (auto x : arr)
+		cout << x << endl;
 
 }
