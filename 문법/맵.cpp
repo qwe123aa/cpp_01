e88198899d2e3c0429b46a2d2ab0545ea694c3bd
thdud__ <love05061114@gmail.com>
2023-10-17 10:03:04 +0900

    auto는 주로 복잡한 자료형을 대체할 때 사용한다.

--- 문법/맵.cpp
+++ 문법/맵.cpp
@@ -11,8 +11,9 @@ int main(void) {
 	moving["장희수"] = 2000;
 	moving["봉평"] = 1959;
 
-	vector<string, int>::iterator iter;
 	//map은 순서가 없어서 iterator로만 반복문 가능
-	for (iter = moving.begin(); iter != moving.end(); iter++)
+	//first는 key, second는 value
+	//auto는 주로 복잡한 자료형을 대체할 때 사용한다.
+	for (auto iter = moving.begin(); iter != moving.end(); iter++)
 		cout << iter->first << iter-> second << endl;
 }
